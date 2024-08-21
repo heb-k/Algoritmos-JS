fe715fc0bdb7e123a6d2faf1ffd948f25d9744be
Héber Kayke <133585442+heb-k@users.noreply.github.com>
2024-08-21 00:20:30 -0300

    Create DiagonalDifference.js

---
+++ DiagonalDifference.js
@@ -0,0 +1,10 @@
+function diagonalDifference(arr) {
+    let diag1= 0;
+    let diag2= 0;
+    let resultado= 0;
+    for(let i = 0; i < arr.length; i++){
+        diag1 += arr[i][i];
+        diag2 += arr[i][(arr.length - 1) - i];
+    }
+    resultado = Math.abs(diag1- diag2);
+    return resultado;
